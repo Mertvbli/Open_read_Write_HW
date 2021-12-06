b00b145b15875d300e244f131ce66021d0ecd76c
Igor <61599472+Mertvbli@users.noreply.github.com>
2021-12-06 22:08:02 +0300

    Update 3task.py

--- 3task.py
+++ 3task.py
@@ -12,15 +12,14 @@ def result_function(result):
             with open(i, encoding='utf-8') as f:
                 stroki = len(f.readlines())
             with open(i, encoding='utf-8') as f:
-                data[i] = (stroki, f.read().strip())
+                data[i] = [stroki, f.read().strip()]
     data = sorted(data.items(), key=lambda x: x[1])
     for ddnn in data:
         with open("4.txt", "a", encoding='utf-8') as document:
             document.write(f"{ddnn[0]}\n")
             document.write(f"{ddnn[1][0]}\n")
-            document.write(f"{ddnn[1][1]}\n")
-
-
+            document.write(str(f"{ddnn[1][1]}\n"))
+    print("Обработка файлов завершена")
 
 
 result_function(rez)
